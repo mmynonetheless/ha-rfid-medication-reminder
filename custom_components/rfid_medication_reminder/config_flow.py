783be493994e35d305678c1001ebd60637ef61b9
Mmy <152811702+mmynonetheless@users.noreply.github.com>
2026-03-15 02:17:24 -0400

    Update config_flow.py

--- custom_components/rfid_medication_reminder/config_flow.py
+++ custom_components/rfid_medication_reminder/config_flow.py
@@ -1,6 +1,5 @@
 """Config flow for RFID Medication Reminder integration."""
 from homeassistant import config_entries
-from homeassistant.core import callback
 import voluptuous as vol
 
 from .const import DOMAIN
@@ -8,7 +7,7 @@ from .const import DOMAIN
 class RFIDMedicationReminderConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for RFID Medication Reminder."""
 
-    VERSION = 2
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
@@ -18,34 +17,10 @@ class RFIDMedicationReminderConfigFlow(config_entries.ConfigFlow, domain=DOMAIN)
         if user_input is not None:
             return self.async_create_entry(
                 title="RFID Medication Reminder",
-                data=user_input,
+                data={},
             )
 
-        # Show a simple form with no required fields
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({}),
-        )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Get the options flow for this handler."""
-        return OptionsFlowHandler(config_entry)
-
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options flow."""
-
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
             data_schema=vol.Schema({}),
         )
