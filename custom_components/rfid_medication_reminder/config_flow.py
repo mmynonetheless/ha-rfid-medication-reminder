b24bf766b14119539bf4d8c1dda8f409002c9ac0
Mmy <152811702+mmynonetheless@users.noreply.github.com>
2026-03-15 02:46:43 -0400

    Update config_flow.py

--- custom_components/rfid_medication_reminder/config_flow.py
+++ custom_components/rfid_medication_reminder/config_flow.py
@@ -4,23 +4,12 @@ import voluptuous as vol
 
 from .const import DOMAIN
 
-class RFIDMedicationReminderConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for RFID Medication Reminder."""
-
+class RFIDReminderConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-
         if user_input is not None:
-            return self.async_create_entry(
-                title="RFID Medication Reminder",
-                data={},
-            )
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({}),
-        )
+            return self.async_create_entry(title="RFID Reminder", data={})
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
